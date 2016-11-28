d3e6d636c954eb44748f9bb2bef94af5aec695f8
Kyle Renfro <kylerenfro@gmail.com>
2016-11-28 09:28:42 -0600

    fixed f13

--- keyboard-fix.sh
+++ keyboard-fix.sh
@@ -5,3 +5,7 @@ echo 2 > /sys/module/hid_apple/parameters/fnmode
 #swap command/alt
 echo 1 > /sys/module/hid_apple/parameters/swap_opt_cmd
 
+#F13 = Insert
+echo "keycode 191 = Insert" | xmodmap -
+
+
